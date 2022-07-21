112903ca86d58bd0cf10c514dac658c3ee7ee60c
felix <felix.luthman@gmail.com>
2022-07-21 02:27:44 +0200

    fuling 3 electric spegalee

--- emitter/wordEmitter.go
+++ emitter/wordEmitter.go
@@ -73,7 +73,7 @@ func main() {
 	switch env := os.Getenv("DEPLOY_ENVIRONMENT"); env {
 	case "compose", "kubernetes":
 		// host = "receiver"
-		host = "172.17.0.3"
+		host = "172.17.0.4"
 	default:
 		host = "localhost"
 	}
